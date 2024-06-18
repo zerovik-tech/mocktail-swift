2f0054f9f7ccc5c0eb8b0c890700d0fca3748138
Sacchin Pandey <pandeysachin9988@gmail.com>
2024-06-18 14:07:25 +0530

    Initial Commit

---
+++ MocktailIosTemp/MocktailIosTempApp.swift
@@ -0,0 +1,20 @@
+//
+//  MocktailIosTempApp.swift
+//  MocktailIosTemp
+//
+//  Created by Sachin Pandey on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MocktailIosTempApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
